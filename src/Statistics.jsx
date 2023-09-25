1bec82e91c040559e2ca3f64a6b12b47dfbdcbee
Dibbo T <dibbotripura7@gmail.com>
2023-09-25 23:27:36 +0600

    pie chart has been created

--- src/Statistics.jsx
+++ src/Statistics.jsx
@@ -1,9 +1,24 @@
-
+import { PieChart, Pie } from 'recharts';
 
 const Statistics = () => {
+    const data = [
+        { name: 'Geeksforgeeks', students: 400 },
+        { name: 'Technical scripter', students: 700 },
+        { name: 'Geek-i-knack', students: 200 },
+        { name: 'Geek-o-mania', students: 1000 }
+    ];
     return (
         <div>
-            
+            <div className="ml-[30%]">
+                <PieChart width={500} height={500}>
+                    <Pie data={data} dataKey="students" outerRadius={250} fill="green" />  
+                </PieChart>          
+            </div>
+            <div className="flex justify-around my-10">
+                <h3>Your Donation</h3>
+                <h3>Total Donation</h3>
+                </div>
+
         </div>
     );
 };
